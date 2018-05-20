c9eb2a0c692e0de15db56b1b5eaa5ce6207df288
Buddhi Adhikari <buddhiadikari@live.com>
2018-05-20 19:32:36 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports={
-    consumer_key:         'lfOF0NoJ8w3Y4npOvCJrB9n1a',
-    consumer_secret:      'gB4O1SDSllO7hjwxSWG6pwwuprCTjUJde1aXza9oaitsn4gFuk',
-    access_token:         '4762184121-B71KNqXUf2pXJ9p3iv7k5PSqfKZR2CbKxGubOfQ',
-    access_token_secret:  'Doj1WwJE2DjzgJk7dGwvXxjqKNn5ZpxhTxI97VnhgYc8v',
-    // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-}
