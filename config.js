9f1f20453323608f110ad136f61bc79b15efc856
Buddhi Adhikari <buddhiadikari@live.com>
2018-05-20 18:41:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 module.exports={
-    consumer_key:         'U5wlNbO5XukSICSngaIe3t19b',
-    consumer_secret:      'hZNEtahQiCA7Mbt8lWVPsTpGovx9GxrfQAeiPitaiYlmryPoXT',
-    access_token:         '4762184121-B71KNqXUf2pXJ9p3iv7k5PSqfKZR2CbKxGubOfQ',
-    access_token_secret:  'Doj1WwJE2DjzgJk7dGwvXxjqKNn5ZpxhTxI97VnhgYc8v',
+    consumer_key:         'U5w2lNbO5XukSICSngaIe3t19b',
+    consumer_secret:      'hZNEtahCA7Mbt8lWVPsTpGovx9GxrfQAeiPitaiYlmryPoXT',
+    access_token:         '47184121-B71KNqXUf2pXJ9p3iv7k5PSqfKZR2CbKxGubOfQ',
+    access_token_secret:  'Doj1WwJE2DjzgJk7dGwvXqKNn5ZpxhTxI97VnhgYc8v',
     // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
