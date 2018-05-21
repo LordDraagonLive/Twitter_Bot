c58400efafd4455c8a5e4ba27d89e9f78e06f0cd
Buddhi Adhikari <buddhiadikari@live.com>
2018-05-21 22:04:46 +0100

    Added posting images and stream function if someone follows user

--- Post_bot.js
+++ Post_bot.js
@@ -3,20 +3,100 @@ console.log('The Bot is starting!');
 // imports
 var Twit = require('twit');
 var config = require('./config');
+var fs = require('fs');
 
 //get access tokens and keys
 var T = new Twit(config)
 
-var tweet = {
-    status:'#TwitterBot This twitter bot can now post'
+//setting up a new user stream
+var stream = T.stream('user');
+
+//anytime someone follows me call the followed() method
+// stream.on('follow',followed);
+
+//Tweets with an image
+tweetWithAnImage();
+
+//followed function
+function followed(eventMsg){
+    var name = eventMsg.source.name;
+    var screenName = eventMsg.source.screen_name;
+
+    console.log('@'+screenName+'followed you!')
+    tweetIfFollowed('. @'+ screenName + ' Thank you for following, Have a great day!');
 }
 
-T.post('statuses/update',tweet,tweeted);
+//call tweetIt() every 20 seconds
+// tweetIt();
+// setInterval(tweetIt,1000*20);
 
-function tweeted(err, data, response){
-    if(err){
-        console.log('An error occured! BAC!');
-    }else{
-        console.log('Tweeted Succesfully!');
+// posts a tweet with a random number
+function tweetIt(){
+
+    var r = Math.floor(Math.random()*100);
+    
+    var tweet = {
+        status:'#TwitterBot #BotTesting This twitter bot can now post with random numbers '+r
+    }
+
+    T.post('statuses/update',tweet,tweeted);
+
+    function tweeted(err, data, response){
+        if(err){
+            console.log('An error occured! BAC!');
+        }else{
+            console.log('Tweeted Succesfully!');
+        }
+
     }
 }
+
+function tweetIfFollowed(msg){
+
+    var tweet = {
+        status: msg
+    }
+
+    T.post('statuses/update',tweet,tweeted);
+
+    function tweeted(err, data, response){
+        if(err){
+            console.log('An error occured! BAC!');
+        }else{
+            console.log('Tweeted Succesfully!');
+        }
+    }
+}
+
+function tweetWithAnImage(){
+    var filename = 'Assets/imgs/someImage/output.png'
+    var params = {
+        encoding: 'base64'
+    }
+
+    var b64Content = fs.readFileSync(filename,params);
+    T.post('media/upload',{media_data:b64Content},uploaded);
+
+    function uploaded(err, data, response) {
+
+        var mediaIDString = data.media_id_string;
+        var tweet = {
+            status:'#TwitterBot live from Nodejs posting an image using processing 3',
+            media_ids:[mediaIDString]
+        }
+
+        T.post('statuses/update',tweet,tweeted);
+
+    }
+
+    function tweeted(err, data, response){
+        if(err){
+            console.log('An error occured! BAC!');
+        }else{
+            console.log('Tweeted Succesfully!');
+        }
+    }
+}
+
+
+// console.log('The Bot stopped succesfully!');
