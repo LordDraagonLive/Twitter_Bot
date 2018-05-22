06181680e7d11556dea867b0b6982c41349d3c1c
Buddhi Adhikari <buddhiadikari@live.com>
2018-05-22 10:20:52 +0100

    .gitignore is now working

--- config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports={
-    consumer_key:         'a2WuEE7L7gBPVsoMPqhkmTuB9',
-    consumer_secret:      'SuxzoBbw4FetdM3o3cUzDIxOg0uqPG1BVXsO8ZoHC3Iidscu5A',
-    access_token:         '4762184121-B71KNqXUf2pXJ9p3iv7k5PSqfKZR2CbKxGubOfQ',
-    access_token_secret:  'Doj1WwJE2DjzgJk7dGwvXxjqKNn5ZpxhTxI97VnhgYc8v',
-    // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests
-}
